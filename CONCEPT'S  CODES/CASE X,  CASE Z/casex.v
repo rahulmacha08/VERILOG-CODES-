98e05d5dd61b0b8c8d33229cbd0cf5d2bf213044
Rahul macha <macharahul780@gmail.com>
2026-05-29 15:50:30 +0530

    Add files via upload

---
+++ CONCEPT'S  CODES/CASE X,  CASE Z/casex.v
@@ -0,0 +1,40 @@
+module casee(
+             input [3:0] din ,
+			 output reg [1:0] dout,
+			 output reg valid
+			 );
+	
+	always @(*) begin 
+	valid =1'b1;
+	casex(din)
+	4'b1xxx:dout=2'b11;
+	4'b01xx:dout=2'b01;
+	4'b001x:dout=2'b10;
+	4'b0001:dout=2'b00;
+	default : begin 
+	dout=2'bxx;
+	valid=1'b0;
+	end
+	endcase
+	end 
+endmodule 
+
+//testbench 
+module tb;
+reg [3:0] din;
+wire [1:0] dout;
+wire valid;
+
+casee uut(din,dout,valid);
+initial begin  
+$monitor("din=%0b,dout=%0b,valid=%0b,t=%0t",din,dout,valid,$time);
+din=4'b0000;#3;
+din=4'b0101;#3;
+din=4'b1100;#3;
+din=4'b1110;#3;
+din=4'b0010;#3;
+din=4'b1001;#3;
+din=4'b1111;#3;
+#100 $finish;
+end 
+endmodule 
